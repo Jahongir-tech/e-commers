17ec5f663640ecfda22bf4969c5eec28e3972c0c
Jakhongirbey <q.jakhongirbey@gmail.com>
2025-02-09 21:28:49 +0500

    product

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -2,50 +2,50 @@ import { IProduct } from "../../types";
 import Stars from "../stars/Stars";
 import LikeBtn from "./LikeBtn";
 import SeeBtn from "./SeeBtn";
-// import MyLoader from "./Skeleton";
 
 const Products = ({ data, title }: { data: any; title: any }) => {
-    const productItems = data?.products?.map((product: IProduct) => (
-        <div key={product.id} className="w-[290px] h-[322px]">
-            <div className="h-[250px] bg-[#F5F5F5] flex justify-center items-center relative group overflow-hidden rounded-sm">
-                <img
-                    src={
-                        product.images[1]
-                            ? product.images[1]
-                            : product.images[0]
-                    }
-                    alt={product.title}
-                    className="object-contain w-full h-[200px]"
-                />
-                <div className="absolute top-2 flex flex-col gap-2 right-[-50px] group-hover:right-2 opacity-0 group-hover:opacity-100 duration-300 ease-in-out">
-                    <LikeBtn product={product} />
-                    <SeeBtn product={product} />
+    const productItems = data?.products
+        ?.slice(0, 8)
+        .map((product: IProduct) => (
+            <div key={product.id} className="w-[290px] h-[322px]">
+                <div className="h-[250px] bg-[#F5F5F5] flex justify-center items-center relative group overflow-hidden rounded-sm">
+                    <img
+                        src={
+                            product.images[1]
+                                ? product.images[1]
+                                : product.images[0]
+                        }
+                        alt={product.title}
+                        className="object-contain w-full h-[200px]"
+                    />
+                    <div className="absolute top-2 flex flex-col gap-2 right-[-50px] group-hover:right-2 opacity-0 group-hover:opacity-100 duration-300 ease-in-out">
+                        <LikeBtn product={product} />
+                        <SeeBtn product={product} />
+                    </div>
+                    <div className="absolute bottom-[-50px] group-hover:bottom-0 duration-300 ease-in-out"></div>
                 </div>
-                <div className="absolute bottom-[-50px] group-hover:bottom-0 duration-300 ease-in-out"></div>
-            </div>
 
-            <div className="flex flex-col pt-4">
-                <h2 className="truncate font-poppins font-medium">
-                    {product.title}
-                </h2>
-                <div className="flex gap-2 font-poppins">
-                    <p className="text-[#DB4444] font-semibold">
-                        ${product.price}
-                    </p>
-                    <Stars rating={product.rating} />
-                    <p className="font-medium">({product.stock})</p>
+                <div className="flex flex-col pt-4">
+                    <h2 className="truncate font-poppins font-medium">
+                        {product.title}
+                    </h2>
+                    <div className="flex gap-2 font-poppins">
+                        <p className="text-[#DB4444] font-semibold">
+                            ${product.price}
+                        </p>
+                        <Stars rating={product.rating} />
+                        <p className="font-medium">({product.stock})</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    ));
+        ));
 
     return (
         <>
             <h2 className="font-poppins-bold text-2xl mb-6 text-center text-gray-900 max-[620px]:text-xl">
                 {title}
             </h2>
-            <div className="container flex flex-wrap justify-start gap-[49.3px] my-7">
-                {/* <MyLoader /> */}
+            <div className="container grid grid-cols-2 md:grid-cols-4 gap-6 my-7">
                 {productItems}
             </div>
         </>
--- src/pages/wishlist/EmptyWishlist.tsx
+++ src/pages/wishlist/EmptyWishlist.tsx
@@ -1,16 +1,10 @@
 import { useNavigate } from "react-router-dom";
-// import emptyWishlistImage from "@/assets/images/wish-list.png";
 
 const EmptyWishlist = () => {
     const navigate = useNavigate();
 
     return (
         <div className="flex flex-col items-center justify-center text-center p-6 sm:p-8 h-[500px] sm:h-[600px]">
-            {/* <img
-        src={emptyWishlistImage}
-        alt="Empty Wishlist"
-        className="w-32 sm:w-48 md:w-60 mb-6 opacity-80"
-      /> */}
             <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-800 dark:text-white mb-2 sm:mb-4">
                 Your Wishlist is Empty
             </h1>
@@ -21,7 +15,7 @@ const EmptyWishlist = () => {
             </p>
 
             <button
-                className="px-3 sm:px-4 py-1 lg:text-2xl lg:font-medium sm:py-2 bg-[#B88E2F] text-white rounded-md shadow-md hover:bg-[#a07424] transition duration-300 text-sm sm:text-base"
+                className="px-3 sm:px-4 py-1 lg:text-2xl lg:font-medium sm:py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 transition duration-300 text-sm sm:text-base"
                 onClick={() => navigate("/")}
             >
                 Go Home
